3e8f0b35182d2416fb1eb2459c3574bce3d8cb75
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-06-27 11:48:40 +0000

    Deploy anjoshigor/carbonidevs.github.io to github.com/anjoshigor/carbonidevs.github.io.git:gh-pages

---
+++ static/js/main.0d114ee1.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{3:function(n,e,t){n.exports=t(9)},9:function(n,e,t){"use strict";t.r(e);var r=t(0),o=t.n(r),a=t(2);var c=function(){return o.a.createElement("h1",null,"Carbonidevs")};t.n(a).a.render(o.a.createElement(c,null),document.getElementById("root"))}},[[3,1,2]]]);
+//# sourceMappingURL=main.0d114ee1.chunk.js.map
